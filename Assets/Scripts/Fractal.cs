b8cec223a149588df29663cf4ccb81873bbfa10b
Rachata Pumma <areeya.phumma@gmail.com>
2023-03-08 16:55:08 +0700

    ddd

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -11,20 +11,37 @@ public class Fractal : MonoBehaviour
     private int iteration;
 
     [SerializeField]
-    private float size;
+    private float size,newT = 0;
 
+    List<GameObject> kelly = new List<GameObject>();
     void Start()
     {
+       
         //  This statement create a game object given a prefab, position and rotation.
-        Instantiate(
-            //  This parameter sets a prefab for an instance to be created.
-            cubePrefab,
-
-            //  This parameter sets an instance position to (0, 0, 0).
-            Vector3.zero,
-
-            //  This parameter sets an instance rotation to (0, 0, 0).
-            Quaternion.identity
-        );
+        Instantiate(cubePrefab, new Vector3(0, 0, 0),Quaternion.identity);
+        cubePrefab.transform.localScale = new Vector3(size,size,size);
+        newT = (size)+2;
+        makeAgain(iteration);
     }
+    void makeAgain(int iteration)
+    {
+        for (int i = 1; i < iteration; i++)
+        {
+
+            Instantiate(cubePrefab, new Vector3(newT, 0, 0), Quaternion.identity);
+            cubePrefab.transform.localScale = new Vector3(size, size, size);
+           kelly.Add(cubePrefab);
+          
+                foreach(var cubePrefab in kelly)
+            {
+                Destroy(cubePrefab);
+            }
+
+
+            newT += size*2;
+        }
+
+      
+    }
+    
 }
